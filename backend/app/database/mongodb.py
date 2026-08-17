1bba7022bfb0f47b14b3376edc029a6f8b40c2de
Albin Sunny <albinsunny564@gmail.com>
2026-08-17 23:09:47 +0530

    Use environment variable for MongoDB connection

--- backend/app/database/mongodb.py
+++ backend/app/database/mongodb.py
@@ -1,3 +1,29 @@
+# from motor.motor_asyncio import AsyncIOMotorClient
+
+# client: AsyncIOMotorClient | None = None
+# database = None
+
+
+# async def connect_to_mongo():
+#     global client, database
+
+#     client = AsyncIOMotorClient("mongodb://localhost:27017")
+
+#     database = client["automlops"]
+
+#     print("✅ Connected to MongoDB")
+
+
+# async def close_mongo_connection():
+#     global client
+
+#     if client:
+#         client.close()
+#         print("❌ MongoDB Connection Closed")
+
+
+
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
 client: AsyncIOMotorClient | None = None
@@ -7,7 +33,12 @@ database = None
 async def connect_to_mongo():
     global client, database
 
-    client = AsyncIOMotorClient("mongodb://localhost:27017")
+    mongo_uri = os.getenv(
+        "MONGO_URI",
+        "mongodb://localhost:27017"
+    )
+
+    client = AsyncIOMotorClient(mongo_uri)
 
     database = client["automlops"]
 
